59cd93aee28ed42327455444859e0e39126b43c6
Tachi <zaveruha007@gmail.com>
2015-06-05 12:31:05 +0300

    Initial CSS & HTML Structure

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>New Web Project</title>
+        <title>YouSearch</title>
         
         <link rel="stylesheet" href="css/style.css" />
         
@@ -11,6 +11,25 @@
         <script src="js/script.js"></script>
     </head>
     <body>
-        <h1>New Web Project Page</h1>
+        <div id="container">
+        	<header>
+        		<h1><span>You</span>Search</h1>
+        		<p>Search Youtube Videos</p>
+        	</header>
+        	<section>
+        		<form id="search=form" name="search-form">
+        			<div class="field-container">
+        				<input type="search" id="query" class="search-field" placeholder="Search..." />
+        				<input type="submit" name="search-btn" id="search-btn" value="" />
+        			</div>
+        			
+        			<ul id="results"></ul>
+        			<div id="button"></div>
+        		</form>
+        	</section>
+        	<footer>
+        		<p>Copyright &copy; 2015, GZ</p>
+        	</footer>
+        </div>
     </body>
 </html>
