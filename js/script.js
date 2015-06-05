94d80bee3899552dbadd978af6e21a1fb531460d
Tachi <zaveruha007@gmail.com>
2015-06-05 13:44:28 +0300

    JS animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 /**
  * @author Gleb Zaveruha
  */
-
-var form = document.getElementById("quizForm");
-form.addEventListener("submit", submitAnswers);
+"use strict";
+$(function(){
+	
+	
+	var form = document.getElementById("search-form");
+	var searchField = $('#query');
+	var icon = $('#search-btn');
+	
+	//Focus Handler
+	$(searchField).on('focus',function(){
+		$(this).animate({
+			width:'100%'
+		},400);
+		$(icon).animate({
+			right: '10px'
+		}, 400);
+	});
+	
+	form.addEventListener("submit", search);
+	
+	
+	
+	
+});
